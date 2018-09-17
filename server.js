1384bc2c55220287ab097f8a030f5f950963f32f
Yatsunami_Yakult <43296913+Hirokazu-Yakult@users.noreply.github.com>
2018-09-17 14:17:57 +0900

    Update server.js

--- server.js
+++ server.js
@@ -13,14 +13,44 @@ const line_config = {
     verify: true
 };
 
-// -----------------------------------------------------------------------------
-// Webサーバー設定
-server.listen(process.env.PORT || 3000);
-
-
-// -----------------------------------------------------------------------------
-// ルーター設定
-server.post('/webhook', line.middleware(line_config), (req, res, next) => {
-    res.sendStatus(200);
-    console.log(req.body);
+const linebotParser = bot.parser();
+app.post('/', linebotParser);
+ 
+bot.on('message', function (event) {
+  event.reply([
+    
+{
+  "type": "template",
+  "altText": "this is a image carousel template",
+  "template": {
+      "type": "image_carousel",
+      "columns": [
+          {
+            "imageUrl": "https://2.bp.blogspot.com/-BaWGrzEtU6k/W4nphK6gdpI/AAAAAAABOQc/K7kSGUhwnjgkOxsHy96fy5B_kGYtcsJiACLcBGAs/s800/yumekawa_girl.png",
+            "action": {
+              "type": "message",
+              "label": "ブジデス",
+    　　　　"text": "ブジデス"
+            }
+          },
+          {
+            "imageUrl": "https://3.bp.blogspot.com/-xhpgn0lt3Wc/VCkbPlPIc8I/AAAAAAAAnDk/IfnflzkRqUw/s800/pet_byouki_cat.png",
+            "action": {
+              "type": "message",
+              "label": "ダメデス",
+              "text": "ダメデス"
+            }
+          }
+      ]
+  }
+}
+    
+  ]).then(function (data) {
+    console.log('Success', data);
+  }).catch(function (error) {
+    console.log('Error', error);
+  });
+});
+app.listen(process.env.PORT || 80, function () {
+  console.log('LineBot is running.');
 });
